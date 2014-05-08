876e9a5b378ce5e2a3f0524482940c318566d606
petun <petun911@gmail.com>
2014-05-09 01:29:11 +0400

    Delete debug and fix clearup form

--- js/callme.js
+++ js/callme.js
@@ -1,10 +1,7 @@
 $(function(){
-	console.log('tst');
-
-	$('#callMeSubmit').click(function(){
+	$('#callMeSubmit').click(function(){		
 		var status = $('#callMeStatus');
-		status.attr('class','call-form--status call-form--status__loading');
-		status.text('');
+		status.attr('class','call-form--status call-form--status__loading').text('');		
 
 		var data = $('.pcallme-form--form').serialize();
 
@@ -14,11 +11,12 @@ $(function(){
 			dataType: 'json',
 			data: data,
 			success:function(r) {
-				console.log(r);
+				//console.log(r);
+				var status = $('#callMeStatus');
 				status.removeClass('call-form--status__loading');
 				if (r.r) {
 					status.addClass('call-form--status__ok');
-					window.setTimeout(function(){$('#callMe').modal('hide')}, 3000);
+					window.setTimeout(function(){ closeCallMeModal(); }, 3000);
 				} else {
 					status.addClass('call-form--status__err');						
 				}
@@ -28,5 +26,12 @@ $(function(){
 		return false;
 	});
 
+
+	function closeCallMeModal() {
+		$('#callMe').modal('hide');
+		$('#callMe input[type=text]').val('');
+		$('#callMe textarea').val('');		
+		$('#callMeStatus').attr('class','call-form--status').text('');		
+	}
 	
 });
